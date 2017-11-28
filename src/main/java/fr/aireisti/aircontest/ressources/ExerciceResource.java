88cef01c180c6ab17ad79a450f7acf04be7631ec
Dracarys <laurine.sorel@free.fr>
2017-11-28 16:20:19 +0100

    [/exercice] Starting @get

--- src/main/java/fr/aireisti/aircontest/ressources/ExerciceResource.java
+++ src/main/java/fr/aireisti/aircontest/ressources/ExerciceResource.java
@@ -2,6 +2,7 @@ package fr.aireisti.aircontest.ressources;
 
 import fr.aireisti.aircontest.Hibernate.HibernateUtil;
 import fr.aireisti.aircontest.models.Exercice;
+import fr.aireisti.aircontest.models.State;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -9,12 +10,24 @@ import javax.ws.rs.core.MediaType;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import java.util.Calendar;
 
 @Path("/exercice")
 public class ExerciceResource {
 
     private Session session;
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Exercice getExerciceById(@PathParam("id") String id){
+        session = HibernateUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("SELECT e FROM  Exercice e WHERE id=:id");
+        Exercice exercice = new Exercice();
+        return exercice;
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
