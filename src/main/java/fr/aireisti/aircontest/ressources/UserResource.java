a22b88b43270cf75b24496fddd843d9898c87eb5
Elio Maisonneuve <elio.maisonneuve@eisti.fr>
2018-01-14 08:15:43 +0100

    allow access to /users only to Admin people

--- src/main/java/fr/aireisti/aircontest/ressources/UserResource.java
+++ src/main/java/fr/aireisti/aircontest/ressources/UserResource.java
@@ -3,26 +3,35 @@ package fr.aireisti.aircontest.ressources;
 
 import fr.aireisti.aircontest.Hibernate.HibernateUtil;
 import fr.aireisti.aircontest.models.User;
+import fr.aireisti.aircontest.security.Secured;
 import org.hibernate.Session;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Path("/user")
+@Path("/users")
 public class UserResource {
 
     @GET
+    @Secured
     @Produces({MediaType.APPLICATION_JSON})
-    public List<User> getUsers() {
-        Session hibernateSession;
-        hibernateSession = HibernateUtil.getSessionFactory().openSession();
-        List<User> users = hibernateSession.createQuery("FROM User").list();
-        hibernateSession.close();
-        return users;
+    public List<User> getUsers(@Context SecurityContext securityContext) {
+        if (securityContext.isUserInRole("Admin")) {
+            Session hibernateSession;
+            hibernateSession = HibernateUtil.getSessionFactory().openSession();
+            List<User> users = hibernateSession.createQuery("FROM User").list();
+            hibernateSession.close();
+            return users;
+        }
+
+        throw new NotAuthorizedException("");
     }
 }
